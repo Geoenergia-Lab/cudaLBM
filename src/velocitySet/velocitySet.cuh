511f642ad9971f3dd0e8b2551659c9a0a5014114
kreeyto <royal.ssamy@gmail.com>
2026-02-24 13:55:18 -0300

    feat: borrow isothermal assumption from ViniDevelop2

--- src/velocitySet/velocitySet.cuh
+++ src/velocitySet/velocitySet.cuh
@@ -197,7 +197,7 @@ namespace LBM
 
         __device__ __host__ [[nodiscard]] static inline scalar_t delta_m(const thread::array<scalar_t, NUMBER_MOMENTS<false>()> &moments) noexcept
         {
-            return static_cast<scalar_t>(1) / static_cast<scalar_t>(3) *
+            return cs2<scalar_t>() *
                    (moments[m_i<1>()] * moments[m_i<1>()] +
                     moments[m_i<2>()] * moments[m_i<2>()] +
                     moments[m_i<3>()] * moments[m_i<3>()] -
@@ -206,7 +206,7 @@ namespace LBM
 
         __device__ __host__ [[nodiscard]] static inline scalar_t delta_m(const thread::array<scalar_t, NUMBER_MOMENTS<true>()> &moments) noexcept
         {
-            return static_cast<scalar_t>(1) / static_cast<scalar_t>(3) *
+            return cs2<scalar_t>() *
                    (moments[m_i<1>()] * moments[m_i<1>()] +
                     moments[m_i<2>()] * moments[m_i<2>()] +
                     moments[m_i<3>()] * moments[m_i<3>()] -
